17e36c3d5447d7817934537a55a23e0194775e52
Vikash kumar <bindukumar480@gmail.com>
2025-06-29 16:05:27 +0530

    Update main.py

--- main.py
+++ main.py
@@ -22,7 +22,7 @@ def is_valid_url(text):
 
 @bot.on_message(filters.command("start"))
 async def start(_, m: Message):
-    await m.reply("Vikash uploader bot".)
+    await m.reply("Vikash uploader bot")
 
 Send a .txt file with Classplus video links.
 Bot will auto-download and send you the videos (Premium only).""")
